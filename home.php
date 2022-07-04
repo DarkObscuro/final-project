b58e70743004ced5cb5215ffd19458a358f9be48
Nicolas <nicolas_thiphaine@yahoo.fr>
2022-07-04 10:51:42 +0900

    model update

--- home.php
+++ home.php
@@ -1,16 +1,10 @@
 <?php include 'view/header.php'; ?>
 <main>
-    <h1>Selected Product List</h1>
+    <h1>Inserted Player List</h1>
     <ul>
-    <?php foreach ($products as $product) : ?>
-        <li> <?php echo $product['productName']; ?></li>
-    <?php endforeach; ?>
+        <li> <?php echo 'Id: ', $player['playerID']; ?></li>
+        <li> <?php echo 'Pseudo: ', $player['playerPseudo']; ?></li>
+        <li> <?php echo 'Job: ', $player['playerJob']; ?></li>
     </ul>
-
-    <h1>Delete Message</h1>
-    <p> <?php echo $delete_message,$deleted_product_id; ?></p>
-
-    <h1>Insert Message</h1>
-    <p> <?php echo $insert_message,$inserted_product_id; ?></p>
 </main><!-- end content -->
 <?php include 'view/footer.php'; ?>
--- index.php
+++ index.php
@@ -2,57 +2,22 @@
 require('util/main.php');
 
 require('model/database.php');
-require('model/product_db.php');
+require('model/player_db.php');
+
+/***************************************
+ * Insert a player
+ ****************************************/
+
+// Sample data
+$player_ID = 1;
 
 /*********************************************
- * Select some products
+ * Select some player
  **********************************************/
 
-// Sample data
-$cat_id = 2;
+// Get the player
+$player = get_player($player_ID);
 
-// Get the products
-$product = get_products_by_category($cat_id);
-
-/***************************************
- * Delete a product
- ****************************************/
-
-// Sample data
-$product_name = 'Fender Telecaster';
-
-// Delete the product and display an appropriate messge
-$deleted_product_id = get_product_by_name($product_name)[0][0];
-
-if ($deleted_product_id == NULL || $deleted_product_id == FALSE) {
-    $delete_message = "No rows were deleted.";
-} else {
-    delete_product($deleted_product_id);
-    $delete_message = "Fender Telecaster product was deleted successfully. Deleted product ID: ";
-}
-
-/***************************************
- * Insert a product
- ****************************************/
-
-// Sample data
-$category_id = 1;
-$code = 'tele';
-$name = 'Fender Telecaster';
-$description = 'NA';
-$price = '949.99';
-
-// Insert the data
-$discount_percent = 42.00;
-$inserted_product_id = add_product($category_id, $code, $name, $description, $price, $discount_percent);
-
-// Display an appropriate message
-
-if ($inserted_product_id == NULL || $inserted_product_id == FALSE) {
-    $insert_message = "No rows were inserted. The code you used might already be taken. Check Database Error message for more information.";
-} else {
-    $insert_message = "Fender Telecaster product was inserted successfully. Inserted product ID: ";
-}
 
 include 'home.php';
 ?>
