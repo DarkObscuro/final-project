233ec48d134359f12be43edf27547c3f86c641a4
Nicolas <nicolas_thiphaine@yahoo.fr>
2022-07-05 11:26:20 +0900

    Manager redirection

---
+++ controller/manager/menu.php
@@ -0,0 +1,16 @@
+<?php include '../../view/header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="?action=player_manager">Player Manager</a>
+        </li>
+        <li>
+            <a href="?action=team_manager">Team Manager</a>
+        </li>
+        <li>
+            <a href="?action=raid_manager">Raid Manager</a>
+        </li>
+    </ul>
+</main>
+<?php include '../../view/footer.php'; ?>
---
+++ controller/manager/teams.php
@@ -0,0 +1,5 @@
+<?php include '../../view/header.php'; ?>
+<main>
+    <h1>Teams</h1>
+</main>
+<?php include '../../view/footer.php'; ?>
