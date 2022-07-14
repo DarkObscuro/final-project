2372941ade324af49f853ed373737c012321eab1
Nicolas <nicolas_thiphaine@yahoo.fr>
2022-07-14 21:44:25 +0900

    Home Page

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php 
 require('util/main.php');
 include ('view/header_home.php'); ?>
-<main>
-    <h1>Menu</h1>
-    <ul>
-        <li>
-            <a href="controller/manager">Manager</a>
-        </li>
-        <li>
-            <a href="controller/timetable">Timetable</a>
-        </li>
-        <li>
-            <a href="controller/statistics">Statistics</a>
-        </li>
-    </ul>
-</main>
+<h1>Raid Planner</h1>
+<ul>
+    <li><a href="controller/manager" data-text="Manager">Manager</a></li>
+    <li><a href="controller/timetable" data-text="Timetable">Timetable</a></li>
+    <li><a href="controller/statistics" data-text="Statistics">Statistics</a></li>  
+</ul> 
 <?php include 'view/footer.php'; ?>
--- view/header_home.php
+++ view/header_home.php
@@ -3,10 +3,9 @@
 <!-- the head section -->
 <head>
     <title>Raid Planner</title>
-    <link rel="stylesheet" type="text/css" href="util/css/main.css?version=1">
+    <link rel="stylesheet" type="text/css" href="util/css/index.css?version=1">
     <link rel="icon" type="image/x-icon" href="image/favicon.ico">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Raid Planner</h1></header>
