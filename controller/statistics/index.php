412425a05dd00c2af5f5ad384e8a6a612a8f0f95
Nicolas <nicolas_thiphaine@yahoo.fr>
2022-07-14 23:31:56 +0900

    chart values update

--- controller/statistics/index.php
+++ controller/statistics/index.php
@@ -23,25 +23,25 @@ foreach($roles_count as $role_count){
 }
 
 $dataPoints3 = array();
-array_push($dataPoints3, array("label"=> 'Paladin', "y"=> rand(5366, 5681), "color"=> '#A8D2E6'));
-array_push($dataPoints3, array("label"=> 'Warrior', "y"=> rand(5295, 5608), "color"=> '#cf2621'));
-array_push($dataPoints3, array("label"=> 'Dark Knight', "y"=> rand(5644, 5923), "color"=> '#d126cb'));
-array_push($dataPoints3, array("label"=> 'Gunbreaker', "y"=> rand(5651, 5931), "color"=> '#796D30'));
-array_push($dataPoints3, array("label"=> 'White Mage', "y"=> rand(4880, 5295), "color"=> '#FFF0DC'));
-array_push($dataPoints3, array("label"=> 'Scholar', "y"=> rand(4972, 5617), "color"=> '#8657FF'));
-array_push($dataPoints3, array("label"=> 'Astrologian', "y"=> rand(4663, 5461), "color"=> '#FFE74A'));
-array_push($dataPoints3, array("label"=> 'Sage', "y"=> rand(4949, 5324), "color"=> '#80A0F0'));
-array_push($dataPoints3, array("label"=> 'Monk', "y"=> rand(8660, 9240), "color"=> '#d69c00'));
-array_push($dataPoints3, array("label"=> 'Dragoon', "y"=> rand(8565, 9185), "color"=> '#4164CD'));
-array_push($dataPoints3, array("label"=> 'Ninja', "y"=> rand(8749, 9269), "color"=> '#AF1964'));
-array_push($dataPoints3, array("label"=> 'Samurai', "y"=> rand(8710, 9091), "color"=> '#e46d04'));
-array_push($dataPoints3, array("label"=> 'Reaper', "y"=> rand(8330, 8804), "color"=> '#965A90'));
-array_push($dataPoints3, array("label"=> 'Bard', "y"=> rand(7909, 8687), "color"=> '#91BA5E'));
-array_push($dataPoints3, array("label"=> 'Machinist', "y"=> rand(7907, 8242), "color"=> '#6EE1D6'));
-array_push($dataPoints3, array("label"=> 'Dancer', "y"=> rand(7774, 8471), "color"=> '#E2B0AF'));
-array_push($dataPoints3, array("label"=> 'Black Mage', "y"=> rand(8516, 9013), "color"=> '#A579D6'));
-array_push($dataPoints3, array("label"=> 'Summoner', "y"=> rand(8162, 8597), "color"=> '#2D9B78'));
-array_push($dataPoints3, array("label"=> 'Red Mage', "y"=> rand(8043, 8588), "color"=> '#e87b7b'));
+array_push($dataPoints3, array("label"=> 'Paladin', "y"=> rand(5681, 5979), "color"=> '#A8D2E6'));
+array_push($dataPoints3, array("label"=> 'Warrior', "y"=> rand(5608, 5921), "color"=> '#cf2621'));
+array_push($dataPoints3, array("label"=> 'Dark Knight', "y"=> rand(5923, 6254), "color"=> '#d126cb'));
+array_push($dataPoints3, array("label"=> 'Gunbreaker', "y"=> rand(5932, 6205), "color"=> '#796D30'));
+array_push($dataPoints3, array("label"=> 'White Mage', "y"=> rand(5295, 5666), "color"=> '#FFF0DC'));
+array_push($dataPoints3, array("label"=> 'Scholar', "y"=> rand(5617, 6162), "color"=> '#8657FF'));
+array_push($dataPoints3, array("label"=> 'Astrologian', "y"=> rand(5461, 5977), "color"=> '#FFE74A'));
+array_push($dataPoints3, array("label"=> 'Sage', "y"=> rand(5324, 5674), "color"=> '#80A0F0'));
+array_push($dataPoints3, array("label"=> 'Monk', "y"=> rand(9240, 9789), "color"=> '#d69c00'));
+array_push($dataPoints3, array("label"=> 'Dragoon', "y"=> rand(9185, 9751), "color"=> '#4164CD'));
+array_push($dataPoints3, array("label"=> 'Ninja', "y"=> rand(9269, 9843), "color"=> '#AF1964'));
+array_push($dataPoints3, array("label"=> 'Samurai', "y"=> rand(9091, 9415), "color"=> '#e46d04'));
+array_push($dataPoints3, array("label"=> 'Reaper', "y"=> rand(8804, 9352), "color"=> '#965A90'));
+array_push($dataPoints3, array("label"=> 'Bard', "y"=> rand(8687, 9292), "color"=> '#91BA5E'));
+array_push($dataPoints3, array("label"=> 'Machinist', "y"=> rand(8242, 8586), "color"=> '#6EE1D6'));
+array_push($dataPoints3, array("label"=> 'Dancer', "y"=> rand(8471, 9306), "color"=> '#E2B0AF'));
+array_push($dataPoints3, array("label"=> 'Black Mage', "y"=> rand(9013, 9614), "color"=> '#A579D6'));
+array_push($dataPoints3, array("label"=> 'Summoner', "y"=> rand(8597, 9191), "color"=> '#2D9B78'));
+array_push($dataPoints3, array("label"=> 'Red Mage', "y"=> rand(8588, 9286), "color"=> '#e87b7b'));
 ?>
 
 <main>
